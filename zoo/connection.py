2c1986e684dac5474e9a4a7106cf27dc08145661
wei-coding <edward.yu0830@gmail.com>
2023-09-07 23:09:31 +0800

    ephemeral znode

--- zoo/connection.py
+++ zoo/connection.py
@@ -9,7 +9,7 @@ def zoo_client(hosts, port = 2181, name = "a", size = "1K", times=30, logname="t
     for i in range(times):
         f = open("data/"+logname, "a+")
         log_str = f"/{name}{i}{name} {time.time()}"
-        zk.create(f"/{name}{i}{name}", (i%10).to_bytes(1, 'big') * parse_size.parse(size))
+        zk.create(f"/{name}{i}{name}", (i%10).to_bytes(1, 'big') * parse_size.parse(size), ephemeral=True)
         log_str += f" {time.time()}"
         print(log_str, file=f)
         f.close()
